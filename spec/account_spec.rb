95fdfedeea5fe7bba0490dc3d3e0d8413a28937b
Maria Vanaale <mvanaale@gmail.com>
2019-05-22 13:02:48 +0100

    refactored tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,36 +1,38 @@
 require 'account.rb' # frozen_string_literal: true
 
 describe Account do
+  before(:each) do
+    @account = Account.new
+  end
+
   it 'returns empty transaction history' do
-    account = Account.new
-    expect(account.transactions).to eq([])
+    expect(@account.transactions).to eq([])
   end
 
   context 'deposits into account' do
     it 'deposit 100' do
-      account = Account.new
-      account.deposit(100, '01/05/2019')
-      expect(account.transactions).to eq([{ date: '01/05/2019', credit: '100.00', debit: '', balance: '100.00' }])
+      @account.deposit(100, '01/05/2019')
+      expect(@account.transactions).to eq([{ date: '01/05/2019', credit: '100.00', debit: '', balance: '100.00' }])
     end
 
     it 'deposit 200' do
-      account = Account.new
-      account.deposit(200, '02/05/2019')
-      expect(account.transactions).to eq([{ date: '02/05/2019', credit: '200.00', debit: '', balance: '200.00' }])
+      @account.deposit(200, '02/05/2019')
+      expect(@account.transactions).to eq([{ date: '02/05/2019', credit: '200.00', debit: '', balance: '200.00' }])
     end
 
     it 'deposit 250.20' do
-      account = Account.new
-      account.deposit(250.20, '03/05/2019')
-      expect(account.transactions).to eq([{ date: '03/05/2019', credit: '250.20', debit: '', balance: '250.20' }])
+      @account.deposit(250.20, '03/05/2019')
+      expect(@account.transactions).to eq([{ date: '03/05/2019', credit: '250.20', debit: '', balance: '250.20' }])
     end
   end
 
   context 'withdraw from account' do
+    before(:each) do
+      @account.deposit(500, '01/05/2019')
+    end 
+
     it 'withdraws 100' do
-      account = Account.new
-      account.deposit(500, '01/05/2019')
-      account.withdraw(100, '01/05/2019')
+      @account.withdraw(100, '01/05/2019')
       array = [
         { date: '01/05/2019', credit: '500.00', debit: '', balance: '500.00' },
         { date: '01/05/2019', credit: '', debit: '100.00', balance: '400.00' }
@@ -39,9 +41,7 @@ describe Account do
     end
 
     it 'withdraws 200' do
-      account = Account.new
-      account.deposit(500, '01/05/2019')
-      account.withdraw(200, '01/05/2019')
+      @account.withdraw(200, '01/05/2019')
       array = [
         { date: '01/05/2019', credit: '500.00', debit: '', balance: '500.00' },
         { date: '01/05/2019', credit: '', debit: '200.00', balance: '300.00' }
@@ -50,14 +50,12 @@ describe Account do
     end
 
     it 'withdraws 300' do
-      account = Account.new
-      account.deposit(500, '01/05/2019')
-      account.withdraw(300, '01/05/2019')
+      @account.withdraw(300, '01/05/2019')
       array = [
         { date: '01/05/2019', credit: '500.00', debit: '', balance: '500.00' },
         { date: '01/05/2019', credit: '', debit: '300.00', balance: '200.00' }
       ]
-      expect(account.transactions).to eq(array)
+      expect(@account.transactions).to eq(array)
     end
   end
 end
